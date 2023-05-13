1d55306ac82ff88bd81b35a4bc8eedbeecb0cbdb
Pipexlul <felipgg@gmail.com>
2023-05-13 04:34:05 -0400

    Fix port variable name

--- src/utils/envUtils.js
+++ src/utils/envUtils.js
@@ -1,7 +1,7 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const numPort = parseInt(process.env.PORT);
+const numPort = parseInt(process.env.TEST_PORT);
 
 const envConfig = {
   port: isNaN(numPort) ? 3000 : numPort,
